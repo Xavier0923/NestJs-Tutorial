5b57dd2b4b5349a2068bf598062093abb602dd52
Xavierwork01 <xavierwork01@gmail.com>
2022-06-24 10:31:35 +0800

    API URL 網址前綴 + 'api'

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    // URL 網址前綴 + 'api'
+    app.setGlobalPrefix('api');
     await app.listen(3333);
 }
 bootstrap();
